703a2e4f141a1650c607fe5b044b49a7a419f912
raffiihza <raffiihza@github.com>
2026-05-13 20:41:54 +0700

    Revert "feat: add binary converter"
    This reverts commit c26b35606d8dfa4d7391a2b6389b0b4fbc50c169.

--- src/features/binary-converter/index.ts
+++
@@ -1,4 +0,0 @@
-export type { BinaryMode, BinarySeparator, BinaryOptions, BinaryResult } from './types';
-export { BINARY_MODES, BINARY_SEPARATORS, DEFAULT_OPTIONS } from './constants';
-export { processBinary } from './lib/binary-utils';
-export { useBinaryConverter } from './hooks/use-binary-converter';
